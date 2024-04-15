cacca8b5c35cf19fc7269c462e49bcc41966c711
Joseph Brisolara <josephbrisolara3035@gmail.com>
2024-04-15 13:22:02 -0500

    Adding the color table to the database

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,1 +1,17 @@
 package models
+
+type RandomColors struct {
+	ColorID   int
+	ColorName string
+	ColorHex  string
+}
+
+type ColorPalettes struct {
+	PaletteID   int
+	PaletteName string
+	Color1      int
+	Color2      int
+	Color3      int
+	Color4      int
+	Color5      int
+}
